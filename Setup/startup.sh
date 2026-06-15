2bc580a9e4b361acf495be5c354914fe869e6510
Avery-L25 <amland@umich.edu>
2026-06-15 12:17:38 -0400

    setup init and armv8 zwo file

---
+++ Setup/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# declare the camera as a python path to directly grab the appropriate 
+# functions
+export PYTHONPATH=/home/yooperobs/SPRL_Observatory/Sensors/camera/
+
+# run python directly
+/home/yooperobs/py_env/bin/python /home/yooperobs/SPRL_Observatory/system.py
