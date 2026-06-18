1d9d48b5ab34579ab7282fe019db642c7e51e928
Avery-L25 <amland@umich.edu>
2026-06-18 15:01:15 -0400

    device found on port serial0, testing messages

--- Sensors/gps_data_collection.py
+++ Sensors/gps_data_collection.py
@@ -11,6 +11,7 @@ class GPS():
     
     def __init__(self, baud_rate=9600, port="/dev/ttyUSB0") -> None:
 
+        print('Initializing GPS')
         init_list = ['prev_lat','prev_lon','lat','lon']
         
         self.cumulative_distance = 0                                    # todo Read from file.
@@ -25,6 +26,7 @@ class GPS():
 
     def haversine(self, lat1, lon1, lat2, lon2):
         """Calculate the great-circle distance between two points on the Earth."""
+        print(f'haversine function running\nlat1{lat1}\n')
         lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
         dlat = lat2 - lat1
         dlon = lon2 - lon1
@@ -36,6 +38,7 @@ class GPS():
 
     def parse_nmea_sentence(self, sentence):
         """Parse an NMEA sentence to extract GPS data."""
+        print(f'attempting to parse sentence\n{sentence}')
         if sentence.startswith("$GPGGA"):
             parts = sentence.split(',')
             if len(parts) > 9:
@@ -53,6 +56,7 @@ class GPS():
 
     def convert_to_decimal(self, coord, direction):
         """Convert NMEA latitude/longitude to decimal degrees."""
+        print(f"coord:{coord}\ndir:{direction}\nConverting Decimals")
         if not coord or not direction:
             return None
         degrees = int(coord[:2])
@@ -63,6 +67,7 @@ class GPS():
         return decimal
 
     def gpsData(self):
+        print('running gpsData function')
         with serial.Serial(self.port, self.baud_rate, timeout=1) as serial_port:
             # Read a line from the GPS serial data
             data = serial_port.readline().decode('ascii',
@@ -70,6 +75,7 @@ class GPS():
             gps_data = self.parse_nmea_sentence(data)
 
             if gps_data:
+                print(f"{gps_data} \n DATA WAS FOUND")
                 lat, lon = gps_data['latitude'], gps_data['longitude']
                 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
@@ -112,7 +118,7 @@ def main(gps_obj):
             print(f"Cumulative Distance: {cumulative_distance:.2f}"
                     " meters\n")
 
-        time.sleep(1)
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Exiting program...")
 
@@ -121,7 +127,7 @@ def main(gps_obj):
 
 
 if __name__ == "__main__":
-    gps_port = "/dev/ttyUSB0"
+    gps_port = "/dev/serial0"
     baud_rate = 9600
     # make obj and run collection
     gps_obj = GPS(baud_rate=baud_rate,port=gps_port)
